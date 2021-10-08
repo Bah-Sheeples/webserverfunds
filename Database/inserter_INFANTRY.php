e9692a11cbc06503271be7620444f31afa259d19
Andy Su <andy.su@dawsoncollege.qc.ca>
2021-10-08 11:49:03 -0400

    Inserter

---
+++ Database/inserter_INFANTRY.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head> 
+
+        <title>Form </title> 
+
+    </head> 
+
+    <body>
+        <?php=
+            $server = "localhost";
+            $username = "php";
+            $password = "A1f2i3r4e";
+            $database = "COH2UNIT"; 
+
+            $conn = mysqli_connect($server, $username, $password, $database);
+            // Check for successful connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+                }
+            echo "Connected successfully";
+
+            $sql = "INSERT INTO INFANTRY (name, MP_VALUE, faction, type)
+                VALUES htmlspecialchars($_POST['inf_name']), htmlspecialchars($_POST['MP']), htmlspecialchars($_POST['faction']), htmlspecialchars($_POST['type']);";
+            $result = mysqli_query($conn, $sql);
+            echo "<p>" . $result ? "Success!" : "Failure: " . mysqli_error($conn) . "/p>"; 
+        ?>
+
+    </body> 
+
+</html> 
