7fa5a6f2e1bca447f80143c1bc15508751520c4d
Andy Su <andy.su@dawsoncollege.qc.ca>
2021-10-08 12:44:59 -0400

    aa

--- Database/inserter_INFANTRY.php
+++ Database/inserter_INFANTRY.php
@@ -9,7 +9,7 @@
     </head> 
 
     <body>
-        <?php=
+        <?php
             $server = "localhost";
             $username = "php";
             $password = "A1f2i3r4e";
@@ -22,10 +22,11 @@
                 }
             echo "Connected successfully";
 
-            $sql = "INSERT INTO INFANTRY (inf_name, MP_VALUE, faction, type)
-                VALUES {htmlspecialchars($_POST['inf_name'])}, {htmlspecialchars($_POST['MP'])}, {htmlspecialchars($_POST['faction'])}, {htmlspecialchars($_POST['type'])};";
+            $sql = "INSERT INTO INFANTRY (name, MP_VALUE, faction, type)
+                VALUES '{htmlspecialchars($_POST['inf_name'])}', '{htmlspecialchars($_POST['MP'])}', '{htmlspecialchars($_POST['faction'])}', '{htmlspecialchars($_POST['type'])}';";
             $result = mysqli_query($conn, $sql);
-            echo "<p> {$result ? "Success!" : "Failure: " . mysqli_error($conn)} </p>"; 
+            echo <br>;
+            echo "{$result ? "Success!" : "Failure: " . mysqli_error($conn)}"; 
         ?>
 
     </body> 
---
+++ Database/retrieve_INFANTRY.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head> 
+
+        <title>Form: select</title> 
+
+    </head> 
+
+    <body>
+        <?php=
+            $server = "localhost";
+            $username = "php";
+            $password = "A1f2i3r4e";
+            $database = "COH2UNIT"; 
+
+            $conn = mysqli_connect($server, $username, $password, $database);
+            // Check for successful connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+                }
+            echo "Connected successfully";
+
+            $sql = "SELECT * FROM INFANTRY";
+            $result = mysqli_query($conn, $sql);
+
+            foreach($result as $row) {
+            echo "id:'{$row["id"]}' | Name: '{$row["name"]}' | MP_VALUE: {$row["MP_VALUE"]} | Faction: '{$row["faction"]}' | Type: '{$row["type"]}' " <br>; }
+
+            //$sql = "select * from users where {htmlspecialchars($_POST['filter1'])}={htmlspecialchars($_POST['filter2'])}
+
+        ?>
+    </body>
+<html>
+    
