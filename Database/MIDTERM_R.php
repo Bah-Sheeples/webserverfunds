9d4172c3bd396574054d2d0339c9584969e7a64c
Andy Su <andy.su@dawsoncollege.qc.ca>
2021-10-12 11:23:21 -0400

    Update MIDTERM_R.php

--- Database/MIDTERM_R.php
+++ Database/MIDTERM_R.php
@@ -9,29 +9,6 @@
     </head> 
 
     <body>
-        <?php
-            $server = "localhost";
-            $username = "php";
-            $password = "A1f2i3r4e";
-            $database = "COH2UNIT"; 
-
-            $conn = mysqli_connect($server, $username, $password, $database);
-            // Check for successful connection
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-                }
-            echo "Connected successfully";
-            echo "<br/>";
-
-            $sql = "INSERT INTO Products (product_name, num_items)
-                VALUES {htmlspecialchars($_POST['prod'])}, {htmlspecialchars($_POST[num_items])}";
-            $result = mysqli_query($conn, $sql);
-
-            echo "<br/>";
-            echo "{$result ? "Success!" : "Failure: " . mysqli_error($conn)}";
-
-            //$sql = "select * from users where {htmlspecialchars($_POST['filter1'])}={htmlspecialchars($_POST['filter2'])}
-
-        ?>
+        <p>You have requested <?= htmlspecialchars($_GET['num_items'])?> <?= htmlspecialchars($_GET['prod'])?></p> 
     </body>
 <html>
