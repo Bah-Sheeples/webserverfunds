c22b59dd7bf86972c1280374fdaa49d7fe1c708d
Andy Su <andy.su@dawsoncollege.qc.ca>
2021-12-03 11:15:41 -0500

    Update retrieve_INFANTRY.php

--- Database/retrieve_INFANTRY.php
+++ Database/retrieve_INFANTRY.php
@@ -27,23 +27,23 @@
             $faction=htmlspecialchars($_POST["faction"]);
             $type=htmlspecialchars($_POST["type"]);
 
-            // if ($inf_name == "" && isset($faction, $type)) {
-            //     $sql = "SELECT * FROM INFANTRY";
-            // } elseif ($inf_name =="" && isset($type)) {
-            //     $sql = "SELECT * FROM INFANTRY WHERE type='$type'";
-            // } elseif ($inf_name == "" && isset($faction)) {
-            //     $sql = "SELECT * FROM INFANTRY WHERE type='$type'";
-            // } elseif ($inf_name == "") {
-            //     $sql = "SELECT * FROM INFANTRY WHERE faction='$faction' type='$type'";
-            // } elseif (isset($faction)) {
-            //     $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name' type='$type'";
-            // } elseif (isset($faction,$type)) {
-            //     $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name'";
-            // } elseif (isset($type)) {
-            //     $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name' faction='$faction'";
-            // } else {
-            //     $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name' faction='$faction' type='$type'";
-            // }
+             if ($inf_name == "" && isset($faction, $type)) {
+                 $sql = "SELECT * FROM INFANTRY";
+             } elseif ($inf_name =="" && isset($type)) {
+                 $sql = "SELECT * FROM INFANTRY WHERE type='$type'";
+             } elseif ($inf_name == "" && isset($faction)) {
+                 $sql = "SELECT * FROM INFANTRY WHERE type='$type'";
+             } elseif ($inf_name == "") {
+                 $sql = "SELECT * FROM INFANTRY WHERE faction='$faction' type='$type'";
+             } elseif (isset($faction)) {
+                 $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name' type='$type'";
+             } elseif (isset($faction,$type)) {
+                 $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name'";
+             } elseif (isset($type)) {
+                 $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name' faction='$faction'";
+             } else {
+                 $sql = "SELECT * FROM INFANTRY WHERE name='$inf_name' faction='$faction' type='$type'";
+             }
 
             $sql = "SELECT * FROM INFANTRY";
             $result = mysqli_query($conn, $sql);
