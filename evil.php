26c02a17b0ef0e177b72c914bfe2fb8958e815f0
Andy Su <andy.su@dawsoncollege.qc.ca>
2021-12-21 10:53:54 -0500

    finals

---
+++ evil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head> 
+
+        <title>Form: insert</title> 
+    </head> 
+
+    <body>
+        <?php
+            $server = "localhost";
+            $username = "php";
+            $password = "A1f2i3r4e";
+            $database = "EVIL"; 
+
+            $conn = mysqli_connect($server, $username, $password, $database);
+
+            $ip=$_SERVER['REMOTE_ADDR'] 
+            $q=htmlspecialchars($_GET["q"]);
+
+        
+            $sql = "INSERT INTO victims (ip, q) VALUES ('{$ip}', {$q});";
+
+            $url= "https://www.google.com/search?q=" . $q
+            header("Location: $url"); 
+
+        ?>
+
+    </body> 
+
+</html> 
