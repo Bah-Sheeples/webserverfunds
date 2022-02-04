6d55f7f0790516a82e5d6e57212c1b94065ec238
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-02-04 12:32:25 -0500

    Update test.py

--- Project/test.py
+++ Project/test.py
@@ -3,6 +3,7 @@
 import serial
 import time
 import mysql.connector
+from datetime import date
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -19,25 +20,44 @@ ser = serial.Serial (
     timeout=1
 )
 
+light_val=0x0F
+
 while 1:
+
     msg_id = ser.read(1)
     if msg_id == 0x02:
         user_id = ser.read(8)
         pass_id = ser.read(8)
         mycursor = mydb.cursor()
-        sql = "SELECT * FROM users WHERE user = %s password =%s"
+        sqlsel = "SELECT * FROM users WHERE user = %s password =%s"
         try:
-            mycursor.execute(sql,user_id,pass_id)
+            mycursor.execute(sqlsel,user_id,pass_id)
             auth_user = 0x0A    #accepted
             break
         except: 
             auth_user = 0x0D    #denied
-        ser.write(auth_user)
-        break
-    elif msg_id ==0x03:
-       
+        ser.write(auth_user)     
+    elif msg_id == 0x03:
+        today= date.today()
+        mycursor = mydb.cursor()
+        sqlin= "INSERT INTO ir_log (date) VALUES (%s)" 
+        mycursor.execute(sqlin, today)
+        mydb.commit()
+    elif msg_id == 0x04: 
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM light_data")
+        light_val= mycursor.fetchone()
+        ser.write(light_val)
+    
+    
+        
+    
+
 
 # try,except, finally
 # https://www.programiz.com/python-programming/exception-handling#:~:text=Python%20Exception%20Handling%20Using%20try%2C%20except%20and%20finally,with%20else%20clause.%20...%206%20Python%20try...finally.%20
 # Serial
 # https://maker.pro/pic/tutorial/introduction-to-python-serial-ports#:~:text=1%20open%20%28%29%20%E2%80%93%20This%20will%20open%20the,function%20to%20the%20serial%20port%20More%20items...%20 
+# Time
+# 
+
