08b073c14d48e41a000ba316dbbd0e14c8a25b6b
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-02-18 10:32:04 -0500

    Update dbtest.py

--- Project/dbtest.py
+++ Project/dbtest.py
@@ -8,12 +8,12 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-    user_id= "A1234"
-    pass_id= "1234"
-    sqlsel = "SELECT password FROM users WHERE (user = %s)"
-    mycursor.execute(sqlsel,user_id)
-    realpass_id=cursor.fetchone()
-    if realpass_id == pass_id:
-        print("yes")      #accepted
-    else: 
-        print("no")     #denied
+user_id= "A1234"
+pass_id= "1234"
+sqlsel = "SELECT password FROM users WHERE (user = %s)"
+mycursor.execute(sqlsel,user_id)
+realpass_id=cursor.fetchone()
+if realpass_id == pass_id:
+    print("yes")      #accepted
+else: 
+    print("no")     #denied
