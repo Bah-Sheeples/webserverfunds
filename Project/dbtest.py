e89cf28701d798b977191cf702c7fe08ea3d371d
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-02-18 10:30:38 -0500

    Create dbtest.py

---
+++ Project/dbtest.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="pi",
+    password="A1f2i3r4e",
+    database="PROJECT2022"
+)
+
+mycursor = mydb.cursor()
+    user_id= "A1234"
+    pass_id= "1234"
+    sqlsel = "SELECT password FROM users WHERE (user = %s)"
+    mycursor.execute(sqlsel,user_id)
+    realpass_id=cursor.fetchone()
+    if realpass_id == pass_id:
+        print("yes")      #accepted
+    else: 
+        print("no")     #denied
