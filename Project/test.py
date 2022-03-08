656aa6ac2e23e4e83a1df26de6981033cb1d58ea
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-03-08 11:59:45 -0500

    Update test.py

--- Project/test.py
+++ Project/test.py
@@ -69,12 +69,12 @@ while True:
         y-=1
         if y==0:
             print("Light Log")
-            ser.write("2")    #Ready to read
-            ser.write("2")
-            ser.write("2")
-            ser.write("2")
-            ser.write("2")
-            ser.write("2")
+            ser.write("2".encode())    #Ready to read
+            ser.write("2".encode())
+            ser.write("2".encode())
+            ser.write("2".encode())
+            ser.write("2".encode())
+            ser.write("2".encode())
             light_level= ser.read(1)
             int_val = int.from_bytes(light_level,"big")
             now1= datetime.datetime.now()
