bc3106383e6fca651c772ce662cf78341f988b74
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-03-11 10:03:45 -0500

    Update serialtest.py

--- Project/serialtest.py
+++ Project/serialtest.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
+import serial
 
 ser = serial.Serial (
     port='/dev/ttyAMA0',
     baudrate = 2400,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
-    byesize=serial.EIGHTBITS,
-    timeout=1
+    bytesize=serial.EIGHTBITS,
 )
 
-ser.write(0x0F)
-ser.write(0xF0)
-ser.write(0xE0)
+while True
+    ser.write(0x9D)
