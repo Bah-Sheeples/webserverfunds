46b64ef80115f41810991549fe7397d5e50bc359
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-04-01 10:45:28 -0400

    Update test.py

--- Project/test.py
+++ Project/test.py
@@ -158,7 +158,7 @@ while True:
     elif msg_id == 0x07: #Motor
         ser.write(b"\x02")    #Ready to read
         print("Motoring")
-        mydb.disconnect()
+        disconnect()
         db_connect()
         mycursor = mydb.cursor()
         sqlmotor = "SELECT status FROM motor"
