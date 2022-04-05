c7f9fdaa669746340a7c8cff0c53bcd20db1d7ba
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-04-05 10:30:09 -0400

    Update test.py

--- Project/test.py
+++ Project/test.py
@@ -177,19 +177,20 @@ while True:
         sqlmotor = "SELECT status FROM motor"
         mycursor.execute(sqlmotor)
         t_motor = mycursor.fetchone()[0]
-        ser.write(b"\x00")
-        # if drawer_stat==t_motor:    #nothing changed
-        #     ser.write(b"\x01") 
-        #     print("Motor Status: None") 
-        # else:   
-        #     drawer_stat = t_motor
-        #     print("Motor Status:", t_motor)
-        #     if t_motor==1:
-        #         ser.write(b"\x03")  #move open
-        #         print("Open")
-        #     else: 
-        #         ser.write(b"\x02")  #move close
-        #         print("Close")
+        print("Drawer Stat:", drawer_stat)
+        print("t_motor stat:", t_motor)
+        if drawer_stat==t_motor:    #nothing changed
+            ser.write(b"\x01") 
+            print("Motor Status: None") 
+        else:   
+            drawer_stat = t_motor
+            print("Motor Status:", t_motor)
+            if t_motor==1:
+                ser.write(b"\x03")  #move open
+                print("Open")
+            else: 
+                ser.write(b"\x02")  #move close
+                print("Close")
         mycursor.close()
 
     # mycursor = mydb.cursor()
