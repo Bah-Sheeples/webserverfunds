dd84eaa17c6de7d0c6b9097700edde7815a96cbe
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-04-22 11:15:09 -0400

    Update dht_simpletest.py

--- Project/dht_simpletest.py
+++ Project/dht_simpletest.py
@@ -6,7 +6,7 @@ import board
 import adafruit_dht
 
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D18)
+dhtDevice = adafruit_dht.DHT22(board.D6)
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
@@ -24,11 +24,11 @@ while True:
                 temperature_f, temperature_c, humidity
             )
         )
-        TEMP_LOG = open('TEMP_LOG.txt','a')
-        TEMP_LOG.write("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-            temperature_f, temperature_c, humidity)
-        TEMP_LOG.write("\n")
-        TEMP_LOG.close()
+        # TEMP_LOG = open('TEMP_LOG.txt','a')
+        # TEMP_LOG.write("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+        #     temperature_f, temperature_c, humidity)
+        # TEMP_LOG.write("\n")
+        # TEMP_LOG.close()
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
