924b25ba3133b93712aea10f1060676e99cccf6f
Andy Su <andy.su@dawsoncollege.qc.ca>
2022-05-10 12:30:08 -0400

    Update dht_simpletest.py

--- Project/dht_simpletest.py
+++ Project/dht_simpletest.py
@@ -14,26 +14,19 @@ dhtDevice = adafruit_dht.DHT22(board.D6)
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
 while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Failed to retrieve data from humidity sensor")
+
         DHT_LOG = open('DHT_LOG.txt','w')
-        DHT_LOG.write("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
+        DHT_LOG.write("Temp: {:.1f} C    Humidity: {}% ".format(temperature, humidity))
         DHT_LOG.write("\n")
         DHT_LOG.close()
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
+    
     time.sleep(2.0)
 
 # https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/python-setup 
